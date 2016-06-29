9634e2151ac5cf2e737e48d3a2cde41f055ca2d7
Bang Nguyen and Matthew Giem <bang+matthew@>
2016-06-29 15:19:44 -0700

    make our program pass capybara

--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -3,30 +3,30 @@ require("rock_paper_scissors.rb")
 
 describe('String#rock_paper_scissors') do
   it('display true if you have rock and your opponent has scissors') do
-    expect('rock'.rock_paper_scissors("scissors")).to(eq("true"))
+    expect('rock'.rock_paper_scissors("scissors")).to(eq("wins"))
   end
   it('display false if you have rock and your opponent has paper') do
-    expect('rock'.rock_paper_scissors("paper")).to(eq("false"))
+    expect('rock'.rock_paper_scissors("paper")).to(eq("loses"))
   end
   it('display false if you have paper and your opponent has scissors') do
-    expect('paper'.rock_paper_scissors("scissors")).to(eq("false"))
+    expect('paper'.rock_paper_scissors("scissors")).to(eq("loses"))
   end
   it('display true if you have paper and your opponent has rock') do
-    expect('paper'.rock_paper_scissors("rock")).to(eq("true"))
+    expect('paper'.rock_paper_scissors("rock")).to(eq("wins"))
   end
   it('display false if you have scissors and your opponent has rock') do
-    expect('scissors'.rock_paper_scissors("rock")).to(eq("false"))
+    expect('scissors'.rock_paper_scissors("rock")).to(eq("loses"))
   end
   it('display true if you have scissors and your opponent has paper') do
-    expect('scissors'.rock_paper_scissors("paper")).to(eq("true"))
+    expect('scissors'.rock_paper_scissors("paper")).to(eq("wins"))
   end
   it('display tie if you have paper and your opponent has paper') do
-    expect('paper'.rock_paper_scissors("paper")).to(eq("tie"))
+    expect('paper'.rock_paper_scissors("paper")).to(eq("tied"))
   end
   it('display tie if you have rock and your opponent has rock') do
-    expect('rock'.rock_paper_scissors("rock")).to(eq("tie"))
+    expect('rock'.rock_paper_scissors("rock")).to(eq("tied"))
   end
   it('display tie if you have scissors and your opponent has scissors') do
-    expect('scissors'.rock_paper_scissors("scissors")).to(eq("tie"))
+    expect('scissors'.rock_paper_scissors("scissors")).to(eq("tied"))
   end
 end
